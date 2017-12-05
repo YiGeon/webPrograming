6f07a45f57d384b9d5a544f07b61a4a76191dbf9
박이건 <parkegun@gmail.com>
2017-12-06 04:13:08 +0900

    join content not required and Responsive Design

--- models/join.js
+++ models/join.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 var schema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   event: { type: Schema.Types.ObjectId, ref: 'Event' },
-  content: {type: String, trim: true, required: true},
+  content: {type: String},
   numLikes: {type: Number, default: 0},
   createdAt: {type: Date, default: Date.now}
 }, {
